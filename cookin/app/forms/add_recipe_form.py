d94f3faeb3efecd115e741f342428673a5c433f0
Shiying Zheng <shiyingzheng@oberlin.edu>
2015-11-20 01:15:42 -0500

    Tools and dietary restrictions can be added and displayed now.

--- cookin/app/forms/add_recipe_form.py
+++ cookin/app/forms/add_recipe_form.py
@@ -4,6 +4,8 @@ from app.models import Recipe
 
 class AddRecipeForm(ModelForm):
     recipe_tags = CharField(required = False)
+    dietary_restrictions = CharField(required = False)
+    tools = CharField(required = False)
     recipe_ingredients = CharField()
 
     class Meta:
--- cookin/app/views/add_new_recipe_view.py
+++ cookin/app/views/add_new_recipe_view.py
@@ -12,15 +12,26 @@ def add_new_recipe(request):
         form = AddRecipeForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save()
-            tags = form.cleaned_data.get('recipe_tags').split()
+
+            tags = form.cleaned_data.get('recipe_tags').split(',')
             for tag in tags:
                 post.recipe_tags.add(tag)
+
+            tools = form.cleaned_data.get('tools').split(',')
+            for tool in tools:
+                post.tools.add(tool)
+
+            restrs = form.cleaned_data.get('dietary_restrictions').split(',')
+            for restr in restrs:
+                post.dietary_restrictions.add(restr)
+
             ings = form.cleaned_data.get('recipe_ingredients').split("|")
             for ing in ings:
                 data=ing.split(":")
                 ingredient,created = Ingredient.objects.get_or_create(ingredient_name=data[0].lower())
                 relation = Ingredient_In_Recipe(recipe=post, ingredient = ingredient, num_units = data[1])
                 relation.save()
+
             post = form.save()
             return redirect(recipe_detail, recipe_id=post.pk)
         else:
