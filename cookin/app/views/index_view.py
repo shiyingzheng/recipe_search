2f539e69c8d76d0b4045bf2b96ffef0e2172e92e
benjaminpstern <bstern@oberlin.edu>
2015-11-28 05:19:49 -0500

    added recipe list to homepage. next is filter by keywords

--- cookin/app/views/index_view.py
+++ cookin/app/views/index_view.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from app.forms import SearchForm
+from app.models import Recipe
 
 
 def index(request):
     form = SearchForm()
-    return render(request, 'index.html', {'search_form': form})
+    qs = Recipe.objects.all()
+    # keywords = request.GET.get("keywords")
+    return render(request, 'index.html', {'search_form': form, 'qs': qs})
