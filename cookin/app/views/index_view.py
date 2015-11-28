4592efd5ad72c2d0aa8474e51e76f09a4979e87e
benjaminpstern <bstern@oberlin.edu>
2015-11-28 06:06:06 -0500

    added search functionality for keywords on recipe names

--- cookin/app/views/index_view.py
+++ cookin/app/views/index_view.py
@@ -6,5 +6,10 @@ from app.models import Recipe
 def index(request):
     form = SearchForm()
     qs = Recipe.objects.all()
-    # keywords = request.GET.get("keywords")
+    keywords = request.GET.get("keywords")
+    if keywords:
+        keywords = keywords.split()
+        for keyword in keywords:
+            qs = qs.filter(recipe_title__icontains=keyword)
+
     return render(request, 'index.html', {'search_form': form, 'qs': qs})
