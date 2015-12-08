f64c59e15f7e5d3d4223b5e6dc8986b10cc31d18
Cole Peppis <cpeppis@oberlin.edu>
2015-12-08 16:08:29 -0500

    added a static clobal stylesheet and improved index.html

--- cookin/cookin/settings.py
+++ cookin/cookin/settings.py
@@ -119,11 +119,9 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 STATIC_URL = '/static/'
 
-"""
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-"""
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
