fa83f9db322dfdab85b1c1879f1d6e380596074f
Shiying Zheng <shiyingzheng@oberlin.edu>
2015-12-09 00:49:06 -0500

    Added recipe title index too

--- cookin/app/migrations/0019_auto_20151209_0548.py
+++ cookin/app/migrations/0019_auto_20151209_0548.py
@@ -14,6 +14,11 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='ingredient',
             name='ingredient_name',
-            field=models.CharField(db_index=True, max_length=32),
+            field=models.CharField(max_length=32, db_index=True),
+        ),
+        migrations.AlterField(
+            model_name='recipe',
+            name='recipe_title',
+            field=models.CharField(max_length=200, db_index=True),
         ),
     ]
