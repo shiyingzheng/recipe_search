430929fc2c09f888fcb27cf84151fbff0d6a06ee
Aaron Young <aarony422@gmail.com>
2015-12-17 03:28:39 -0500

    Added edit recipe feature for recipes that the current signed in user has created

--- cookin/app/views/recipe_update_view.py
+++ cookin/app/views/recipe_update_view.py
@@ -1,10 +1,78 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.views.generic import UpdateView
-from app.models import Recipe
+from app.views import recipe_detail
+from app.models import Recipe, Ingredient, Ingredient_In_Recipe
 from app.forms import AddRecipeForm
+from django.template import RequestContext
 
-class RecipeUpdateView(UpdateView):
-    model = Recipe
-    template_name = 'recipes/recipe_update.html'
-    form_class = AddRecipeForm
-    success_url = 'app/recipes'
+def recipe_update(request, recipe_id):
+    if request.POST:
+        form = AddRecipeForm(request.POST or None)
+        if form.is_valid():
+            recipe = get_object_or_404(Recipe, pk=recipe_id)
+            form = AddRecipeForm(request.POST, instance = recipe)
+            post = form.save()
+
+            post.recipe_tags.clear()
+            tags = form.cleaned_data.get('recipe_tags').split(',')
+            for tag in tags:
+                post.recipe_tags.add(tag)
+
+            tools = form.cleaned_data.get('tools').split(',')
+            post.tools.clear()
+            for tool in tools:
+                post.tools.add(tool)
+
+            post.dietary_restrictions.clear()
+            restrs = form.cleaned_data.get('dietary_restrictions').split(',')
+            for restr in restrs:
+                post.dietary_restrictions.add(restr)
+
+            post.recipe_ingredients.clear()
+            ings = form.cleaned_data.get('recipe_ingredients').split("|")
+            for ing in ings:
+                data=ing.split(":")
+                ingredient,created = Ingredient.objects.get_or_create(ingredient_name=data[0].lower())
+                relation = Ingredient_In_Recipe(recipe=post, ingredient = ingredient, num_units = data[1])
+                relation.save()
+
+            return redirect(recipe_detail, recipe_id=recipe_id)
+    else:
+        recipe = Recipe.objects.get(pk=recipe_id)
+        ingredients_list = recipe.ingredient_in_recipe_set.all()
+        ingredients = '|'.join([x.ingredient.ingredient_name + ":" + str(x.num_units) for x in ingredients_list])
+        form = AddRecipeForm(instance = recipe)
+
+    return render(request, 'recipes/recipe_update.html',
+        {'recipe': recipe, 'form': form, 'ingredients': ingredients},
+        context_instance=RequestContext(request))
+
+
+
+
+
+
+#    def form_valid(self, form):
+#        post = form.save()
+#
+#        tags = form.cleaned_data.get('recipe_tags').split(',')
+#        for tag in tags:
+#            post.recipe_tags.add(tag)
+#
+#        tools = form.cleaned_data.get('tools').split(',')
+#        for tool in tools:
+#            post.tools.add(tool)
+#
+#        restrs = form.cleaned_data.get('dietary_restrictions').split(',')
+#        for restr in restrs:
+#            post.dietary_restrictions.add(restr)
+#
+#        ings = form.cleaned_data.get('recipe_ingredients').split("|")
+#        for ing in ings:
+#            data=ing.split(":")
+#            ingredient,created = Ingredient.objects.get_or_create(ingredient_name=data[0].lower())
+#            relation = Ingredient_In_Recipe(recipe=post, ingredient = ingredient, num_units = data[1])
+#            relation.save()
+#
+#        post = form.save()
+#        return super(RecipeUpdateView, self).form_valid(form)
