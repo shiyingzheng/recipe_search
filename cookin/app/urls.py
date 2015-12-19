cae9cdcea4ba0fca7c846ec1bfa632aa31c0a6ee
Aaron Young <aarony422@gmail.com>
2015-12-18 21:33:19 -0500

    edited update recipe url

--- cookin/app/urls.py
+++ cookin/app/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     url(r'^register/?$', views.register, name='register'),
     url(r'^login/?$', views.user_login, name='login'),
     url(r'^logout/?$', views.user_logout, name='logout'),
-    url(r'^recipes/update/(?P<recipe_id>[0-9]+)/?$', views.recipe_update, name='recipe_update'),
+    url(r'^recipes/(?P<recipe_id>[0-9]+)/update/?$', views.recipe_update, name='recipe_update'),
 ]
