6060bd6b7710606827a2e8f7ba207a680244775c
benjaminpstern <bstern@oberlin.edu>
2015-12-19 05:10:22 -0500

    testing to see if heroku will work

--- cookin/app/models/recipe_model.py
+++ cookin/app/models/recipe_model.py
@@ -71,7 +71,7 @@ class Recipe(models.Model):
         if cost:
             cost_sum += (cost / self.num_servings)
             num_estimates += 1
-        if len(recipe_ratings == 0):
+        if len(recipe_ratings) == 0:
             return cost_sum / num_estimates
         for rating in recipe_ratings:
             if rating.rating_price:
