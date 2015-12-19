cd76f79868a6e32bdeb43932deac519ae79d800e
Cole Peppis <cpeppis@oberlin.edu>
2015-12-18 22:46:24 -0500

    made the sort by search field pretty!

--- cookin/app/forms/search_form.py
+++ cookin/app/forms/search_form.py
@@ -1,4 +1,4 @@
-from django.forms import Form, CharField
+from django.forms import Form, CharField, ChoiceField
 
 
 class SearchForm(Form):
@@ -7,3 +7,4 @@ class SearchForm(Form):
     max_time = CharField(required=False)
     dietary_restrictions = CharField(required=False)
     max_cost = CharField(required=False)
+    sort_by = ChoiceField(choices=(('rating','Rating'),('cost','Cost'),('time','Time')), required=False)
